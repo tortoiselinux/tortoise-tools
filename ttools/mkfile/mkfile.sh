dd6904bdefba197861d81bb0ca20de7511fda6ce
wellyton-xs <wellyton.offcer@gmail.com>
2024-03-01 10:02:50 -0300

    add file creation with no args.

--- ttools/mkfile/mkfile.sh
+++ ttools/mkfile/mkfile.sh
@@ -2,59 +2,75 @@
 #========================{CABEÇALHO}=======================|
 #
 #AUTOR:
-#       wellyton 'welly' <welly.tohn@gmail.com>
+# wellyton 'welly' <welly.tohn@gmail.com>
 #
 #NOME: mkfile
 #
 #
 #DESCRIÇÃO:
-#   programa para automatizar a criação de arquivos vazios.
+# programa para automatizar a criação de arquivos vazios.
 #
 #LICENÇA:
 # MIT
 #
 #CHANGELOG:
-#   VERSÃO 0.0.1 - beta
+# VERSÃO 0.0.1 - beta
 #
 #   Descrição:
-#   - Criada a funcionalidade de criar arquivos.
+#     - Criada a funcionalidade de criar arquivos.
 #
-#	Alterações:
-#   - criar arquivos vazios
+#	  Alterações:
+#     - criar arquivos vazios
 #
-#   VERSÃO 0.0.2
+# VERSÃO 0.0.2
 #
 #   Descrição:
-#   - gerar scripts a partir do mkfile.
+#     - gerar scripts a partir do mkfile.
 #
 #   Alterações:
-#   - adicionado suporte a argumentos
-#   - gerando scripts por meio do template
+#     - adicionado suporte a argumentos
+#     - gerando scripts por meio do template
 #
 
 # VARIÁVEIS GLOBAIS
 
-msg_help="
+help="
 $(basename $0) {OPÇÃO}
 comandos:
-   $(basename $0) -{comando}
-    h | help
-    e | empity
-    s | script
+  $(basename $0) -{comando}
+  h | help
+  e | empity
+  s | script
 
-    OBS: (-) ou (--) antes dos comandos é opcional
+  OBS: (-) ou (--) antes dos comandos é opcional
 "
+
+# FUNÇÕES
+
+HELP(){
+  printf '%\n' "$help"
+}
+
+CREATE_EMPITY_ARCHIVE(){
+  echo "" > "$1"
+}
 
 # INICIO DO PROGRAMA
 
 case "$1" in
-    h | -h | help | --help)
-        printf '%s\n' "$msg_help"
-    ;;
-    e | -e | empity | --empity)
-        echo "" > "$2"
-    ;;
-    s | -s | script| --script)
-        template "$2"
-    ;;
+  h | -h | help | --help)
+    printf '%s\n' "$help"
+  ;;
+
+  e | -e | empity | --empity)
+    CREATE_EMPITY_ARCHIVE "$2"
+  ;;
+
+  s | -s | script| --script)
+    template "$2"
+  ;;
+
+  *)
+    CREATE_EMPITY_ARCHIVE "$1"
+  ;;
 esac
